18c7731aa94fd79771c312c526f73dcacd68f43d
TetsuyaAdachi <tetsuya_adachi@demia.co.jp>
2023-08-15 23:10:57 +0900

    完成

--- src/pages/purchasePage.tsx
+++ src/pages/purchasePage.tsx
@@ -1,44 +1,49 @@
 import React, { useState } from "react";
-import { PaymentElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import { createPaymentIntent } from "api/pay/paymentIntent";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { createPaymentMethod } from "api/stripe/paymentMethod";
+import { createCard } from "api/pay/card";
+import { createPurchase } from "api/pay/purchase";
+import { useLocation } from "react-router-dom";
 
 export default function PurchasePage() {
   const stripe = useStripe();
   const elements = useElements();
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [errorMessage] = useState(null);
+
+  const search = useLocation().search;
+  const queryParams = new URLSearchParams(search);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    const { error: submitError } = await elements!.submit();
-    if (submitError) {
-      // Show error to your customer
-      setErrorMessage(submitError.message);
-      return;
-    }
-
-    const returnValue = await createPaymentIntent({ amount: 200 });
-    const clientSecret = returnValue["clientSecret"];
-
-    await stripe!.confirmPayment({
-      //`Elements` instance that was used to create the Payment Element
-      elements,
-      clientSecret,
-      confirmParams: {
-        return_url: "https://example.com/order/123/complete",
-      },
+    const cardElement = elements?.getElement("card");
+    if (!(stripe && cardElement)) return;
+    const paymentMethod = await createPaymentMethod(stripe, cardElement, "John");
+    if (!(paymentMethod && paymentMethod["card"])) return;
+    const responseData = await createCard({
+      ex_year: String(paymentMethod["card"]["exp_year"]).substring(2, 4),
+      ex_month: String(paymentMethod["card"]["exp_month"]),
+      card_number: paymentMethod["card"]["last4"],
+      payment_method_id: paymentMethod["id"],
     });
+    const isSuccess = responseData.isSuccess;
+
+    if (isSuccess) {
+      // 購入する
+      const responseData = await createPurchase({
+        chapter: Number(queryParams.get("chapter")),
+      });
+      console.log(responseData);
+    }
   };
 
   return (
     <div>
       <p>カードの登録画面</p>
       <form onSubmit={handleSubmit}>
-        <PaymentElement />
+        <CardElement />
         <button type="submit" disabled={!stripe || !elements}>
           支払う
         </button>
-        {/* Show error message to your customers */}
         {errorMessage && <div>{errorMessage}</div>}
       </form>
     </div>
